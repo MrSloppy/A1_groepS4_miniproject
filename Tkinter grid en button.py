22e9b59dda44a2505a3ce84486d479d19e9791d9
Morgomor <koshinverberne@hotmail.com>
2015-10-13 11:18:53 +0200

    zo werkt tkinter met knoppen(kun je zelf positie bepalen met de left/right enz.)

--- Tkinter grid en button.py
+++ Tkinter grid en button.py
@@ -5,6 +5,7 @@ from tkinter import *
 
 root = Tk()
 
-
+status = Label(root, text = "Preparing to do nothing", bd = 1, relief = SUNKEN, anchor = W)
+status.pack(side = BOTTOM, fill = X)
 
 root.mainloop()#houdt de window in een loop vast zodat het niet weggaat
