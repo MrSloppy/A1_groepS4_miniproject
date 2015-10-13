baa2776e9b36642d97f23cc045ff25691efd99b3
KootGijs <kootgijs@hotmail.com>
2015-10-13 20:13:16 +0200

    Hoofdscherm NS

--- Totaal/NSHoofdscherm.py
+++ Totaal/NSHoofdscherm.py
@@ -5,11 +5,8 @@ TITLE_FONT = ("helvetica", 40, "bold") #
 
  #een foto toevoegen aan dit bestand
 photo1 = PhotoImage( file="Welkomsttekst.png") # je geeft een PhotoImage file een variabele
-photo2 = PhotoImage(file="Maestro.logo.png")
-photo3 = PhotoImage(file="Master.Card.000.logo.png")
-photo4 = PhotoImage(file="V.Pay.logo.png")
 photo5 = PhotoImage(file="Bar.png")
-photo6 = PhotoImage(file="Vlag.Verenigd.Koninkrijk.png")
+
 
 
 root.resizable(width=FALSE, height=FALSE) #je geeft aaan dat je de root(Tk) niet kan veranderen qua grootte
@@ -22,7 +19,8 @@ bottomFrame = Frame(root) # een rechthoekig frame aanbrengen waar je widgets kan
 bottomFrame.pack(side=BOTTOM) # een frame aan de onderkant van het tkinter aanbrengen, hierdoor heb je dus ook gelijk een topframe
 
 label1 = Label(root,height=3, font=TITLE_FONT,  text="Welkom bij NS", bg="#FFCC00", fg="#000066") #je geeft de label die je hebt aangemaakt een font, een achtergrond kleur en een fontkleur
-label1.pack(fill=X)
+label1.pack(fill=X) #je vult de hele x waarde/breedte van de label
+
 label2 = Label(root, image=photo5,  bg="#00246B",  bd = 13,  anchor = W)
 label2.pack(side = BOTTOM)
 
