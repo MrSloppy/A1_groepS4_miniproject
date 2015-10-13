d961be4e2552eef5f6a5f7b6583e004ff5bf0265
Maarten van der Slik <maartenvanderslik@outlook.com>
2015-10-13 13:33:13 +0200

    geen push

---
+++ Totaal/drop down.py
@@ -0,0 +1,10 @@
+__author__ = 'Maarten'
+
+from tkinter import * #tkinter GUI importen
+
+root = Tk() #tkinter scherm aanmaken
+root.resizable(width=FALSE, height=FALSE)
+root.geometry('{}x{}'.format(800, 600))
+root.Listbox
+
+root.mainloop() #een mainloop aan het scherm toevoegen zodat het scherm blijft runnen
